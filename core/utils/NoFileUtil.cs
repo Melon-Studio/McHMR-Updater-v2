43daee0b0d86d250e980c70194e8f08e5b904ebb
fusheng357 <166266777+fusheng357@users.noreply.github.com>
2024-07-19 22:00:20 +0800

    完成了更新相关功能 (#3)
    * 123
    
    * 完成了更新相关功能
    
    * #3 修该工具类的错误命名

---
+++ core/utils/NoFileUtil.cs
@@ -0,0 +1,70 @@
+﻿using McHMR_Updater_v2.core.entity;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Documents;
+
+namespace McHMR_Updater_v2.core.utils
+{
+    public class NoFileUtil
+    {
+         public async Task<List<string>> CheckFiles(List<HashEntity> hashPath, string whiteList, string fileDir)
+        {
+            List<string> notInListFiles = new List<string>();
+            HashSet<string> tempWhiteListSet = new HashSet<string>();
+            string[] whitelistArrayBefore = whiteList.Split(Environment.NewLine.ToCharArray());
+            string[] whitelistArrayAfter = whitelistArrayBefore.Where(s => !string.IsNullOrEmpty(s)).ToArray();
+            HashSet<string> hashPathSet = new HashSet<string>();
+            HashSet<string> whiteListSet = new HashSet<string>();
+
+            foreach (string entry in whitelistArrayAfter)
+            {
+                if (Directory.Exists(entry))
+                {
+                    AddDirectoryFilesToSet(entry, tempWhiteListSet,fileDir);
+                }
+                else
+                {
+                    tempWhiteListSet.Add(entry);
+                }
+            }
+
+            foreach (string entry in tempWhiteListSet)
+            {
+                string relativePath = fileDir + "\\" + entry;
+                whiteListSet.Add(relativePath.Replace("/","\\"));
+            }
+
+           foreach (HashEntity entry in hashPath)
+           {
+               string relativePath = fileDir + "\\" + entry.filePath;
+               hashPathSet.Add(relativePath.Replace("/","\\"));
+           }
+
+            string[] filesInParam3 = Directory.GetFiles(fileDir, "*", SearchOption.AllDirectories);
+
+            foreach (string file in filesInParam3)
+            {
+                if (!hashPathSet.Contains(file) && !whiteListSet.Contains(file))
+                {
+                    notInListFiles.Add(file);
+                }
+            }
+
+            return notInListFiles;
+        }
+
+        private void AddDirectoryFilesToSet(string directory, HashSet<string> fileSet,string fileDir)
+        {
+            string[] files = Directory.GetFiles(fileDir + "\\" + directory, "*", SearchOption.AllDirectories);
+
+            foreach (string file in files)
+            {
+                fileSet.Add(file);
+            }
+        }
+    }
+}
