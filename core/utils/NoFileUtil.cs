df95345c0a3d515efe8a101eda9738d6124ae519
fusheng357 <166266777+fusheng357@users.noreply.github.com>
2024-07-29 17:35:16 +0800

    修复了更新器错误删除文件的bug，增加判断服务器若未增改文件则跳过请求增量包，每次更新时更新器会删除游戏目录下的所有空目录 (#20)
    * 修复了上个版本忘记更新本地版本的bug，并修改了判断更新的逻辑使其能正确判断本地版本是否为在最新版本
    
    * Revert "修复了上个版本忘记更新本地版本的bug，并修改了判断更新的逻辑使其能正确判断本地版本是否为在最新版本"
    
    This reverts commit 27c794770110c23ccb0f7eb6e13e06ee23c594fd.
    
    * 修复了未更新本地版本号的bug，并修改了检测更新的逻辑，使其能够正确判断本地是否为最新版本

--- core/utils/NoFileUtil.cs
+++ core/utils/NoFileUtil.cs
@@ -20,25 +20,31 @@ namespace McHMR_Updater_v2.core.utils
 
             foreach (string entry in whitelistArrayAfter)
             {
-                if (Directory.Exists(entry))
+                string tempPhat = entry;
+                if (tempPhat[0] == '/')
                 {
-                    AddDirectoryFilesToSet(entry, tempWhiteListSet, fileDir);
+                    tempPhat = entry.Substring(1);
+                }
+                tempPhat = tempPhat.Replace('/', '\\');
+                string filePath = fileDir + "\\" + tempPhat;
+                if (Directory.Exists(filePath))
+                {
+                    AddDirectoryFilesToSet(filePath, tempWhiteListSet);
                 }
                 else
                 {
-                    tempWhiteListSet.Add(entry);
+                    tempWhiteListSet.Add(filePath);
                 }
             }
 
             foreach (string entry in tempWhiteListSet)
             {
-                string relativePath = fileDir + "\\" + entry;
-                whiteListSet.Add(relativePath.Replace("/", "\\"));
+                whiteListSet.Add(entry.Replace("/", "\\"));
             }
 
             foreach (HashEntity entry in hashPath)
             {
-                string relativePath = fileDir + "\\" + entry.filePath;
+                string relativePath = fileDir + entry.filePath;
                 hashPathSet.Add(relativePath.Replace("/", "\\"));
             }
 
@@ -54,13 +60,29 @@ namespace McHMR_Updater_v2.core.utils
 
             return notInListFiles;
         }
-        private void AddDirectoryFilesToSet(string directory, HashSet<string> fileSet, string fileDir)
+        private void AddDirectoryFilesToSet(string directory, HashSet<string> fileSet)
         {
-            string[] files = Directory.GetFiles(fileDir + "\\" + directory, "*", SearchOption.AllDirectories);
+            string[] files = Directory.GetFiles(directory, "*", SearchOption.AllDirectories);
 
             foreach (string file in files)
             {
                 fileSet.Add(file);
+            }
+        }
+
+        public async Task RemoveEmptyDirectories(string startLocation)
+        {
+            foreach (var directory in Directory.GetDirectories(startLocation))
+            {
+                // 递归调用以检查子目录
+                await RemoveEmptyDirectories(directory);
+
+                // 如果目录是空的（没有文件和子目录），则删除它
+                if (Directory.GetFiles(directory).Length == 0 && Directory.GetDirectories(directory).Length == 0)
+                {
+                    Directory.Delete(directory);
+                    Console.WriteLine($"Deleted empty directory: {directory}");
+                }
             }
         }
     }
